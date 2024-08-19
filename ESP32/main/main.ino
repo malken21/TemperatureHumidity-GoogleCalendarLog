a50bbb5bebbbb41e57f7de07bf7574f9f4cbd2de
malken21 <77374813+malken21@users.noreply.github.com>
2024-08-20 03:16:45 +0900

    Update main.ino

--- ESP32/main/main.ino
+++ ESP32/main/main.ino
@@ -1,4 +1,7 @@
-#include "DHT.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+#include <time.h>
 #include "config.h"
 
 // DHTセンサー 初期化
@@ -7,16 +10,53 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup()
 {
   Serial.begin(9600);
+
+  // WiFi 接続開始
+  WiFi.begin(SSID, PASSWORD);
+  // WiFi 接続待ち
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  // DHTセンサー 初期化
   dht.begin();
+
+  // 時刻取得のため NTP設定
+  configTime(9 * 3600L, 0, "ntp.nict.jp", "time.google.com", "time.cloudflare.com");
 }
 
 void loop()
 {
-  delay(2000);
-  run();
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain time");
+    delay(1000);
+    return;
+  }
+
+  // 毎時00分にGETリクエストを送信
+  if (timeinfo.tm_min == 0)
+  {
+    // 3回 繰り返す (再試行回数 3回)
+    for (int i = 0; i < 3; i++)
+      if (run())
+        break;
+      else
+        delay(2000);
+    // 次のチェックまで1分待機
+    delay(60000);
+  }
+
+  // 30秒ごとにチェック
+  delay(30000);
 }
 
-void run()
+// 温度と湿度を取得して、GAS にデータを送信する
+// 成功したら true が返される
+// 失敗したら false が返される
+bool run()
 {
   // センサーの値を取得
   float t = dht.readTemperature();
@@ -26,7 +66,7 @@ void run()
   if (isnan(t) || isnan(h))
   {
     Serial.println("Failed to read from DHT sensor!");
-    return;
+    return false;
   }
 
   // 温度
@@ -37,4 +77,5 @@ void run()
   Serial.print("Humidity: ");
   Serial.print(h);
   Serial.println(" %");
+  return true;
 }
